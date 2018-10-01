6a216d81e3dfd0f54fde66dc946b877a75de277e
hariprasath95 <hparthas@buffalo.edu>
2018-10-01 02:13:38 -0400

    priority nested- chain pending

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -337,6 +337,7 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
+  if(thread_current ()->priority == thread_current()->original_priority)
   thread_current ()->priority = new_priority;
   thread_current()->original_priority = new_priority;
   struct thread *hpt = lookup_high_priority_thread(&ready_list);
